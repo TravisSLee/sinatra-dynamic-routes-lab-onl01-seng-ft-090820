ede32a3dcc6f231fac2049c87953bf3fd62b04de
Travis Lee <travisslee223@gmail.com>
2020-10-20 18:04:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do 
     @name = params[:name]
-    
+    "#{@user_name.reverse}"
+  end
+  
+  get '/square/:number' do 
+    @result = params[:number].to_i*params[:number].to_i
+    "#{@results}.to_s"
   end
 end
